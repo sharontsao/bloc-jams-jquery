007e992ccdd1f9882171cf27d10db1edb1ede963
Sharon Tsao <tsao.sh4ron@gmail.com>
2018-11-17 17:33:18 -0600

    assignment 11 completed

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,7 +1,8 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState', player.playState);
+    //$('#time-control .total-time').text( player.prettyTime(player.getDuration()));
   });
 
   $('button#next').on('click', function(){
@@ -9,11 +10,22 @@
 
     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
     const nextSongIndex = currentSongIndex + 1;
-    if (nextSongIndex >= album.songs.length) {return;}
-
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
+
+    if (nextSongIndex >= album.songs.length) {return;}
   });
+
+  $('button#previous').on('click', function () {
+     if (player.playState !== 'playing') {return;}
+
+     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+     const previousSongIndex = currentSongIndex - 1;
+     const previousSong = album.songs[previousSongIndex];
+     helper.playPauseAndUpdate(previousSong);
+
+     if (previousSongIndex <= album.songs.length) {return;}
+   });
 
   $('#time-control input').on('input', function(event) {
     player.skipTo(event.target.value);
@@ -24,9 +36,11 @@
       const currentTime = player.getTime();
       const duration = player.getDuration();
       const percent = (currentTime/duration) * 100;
-      $('#time-control .current-time').text( currentTime );
+      $('#time-control .current-time').text(player.prettyTime(currentTime));
       $('#time-control input').val(percent);
   }, 1000);
 
-
+  $('#volume-control input').on('input', function (event) {
+    player.setVolume(event.target.value);
+  });
 }
